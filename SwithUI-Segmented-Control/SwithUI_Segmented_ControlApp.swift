ac60ffac48171c88192a76c0661da8ecd7442558
louis <louismadrigal26@gmail.com>
2023-06-24 16:14:37 +0800

    Initial Commit

---
+++ SwithUI-Segmented-Control/SwithUI_Segmented_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwithUI_Segmented_ControlApp.swift
+//  SwithUI-Segmented-Control
+//
+//  Created by dev on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SwithUI_Segmented_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
