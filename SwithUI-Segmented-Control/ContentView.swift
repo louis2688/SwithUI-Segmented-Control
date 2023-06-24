0ceaf8d077e14381fb0f8ccde658fa6e6e4ea4ad
louis <louismadrigal26@gmail.com>
2023-06-25 01:44:14 +0800

    Design Segmented control

--- SwithUI-Segmented-Control/ContentView.swift
+++ SwithUI-Segmented-Control/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    init() {
+        UISegmentedControl.appearance().selectedSegmentTintColor = .yellow
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.black], for: .selected)
+    }
+    
+    @State private var selectedSide: SideOfTheForce = .dark
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Picker("Choose a Side" ,  selection: $selectedSide) {
+                    ForEach(SideOfTheForce.allCases, id: \.self ) {
+                        Text($0.rawValue)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
+                
+                Spacer()
+                ChosenHeroView(selectedSide: selectedSide)
+                Spacer()
+            }
+            .navigationTitle("Choose a Side")
         }
-        .padding()
+    }
+}
+
+enum SideOfTheForce: String, CaseIterable {
+    case light = "Light"
+    case grey = "Grey"
+    case dark = "Dark"
+}
+
+struct ChosenHeroView: View {
+    var selectedSide: SideOfTheForce
+    var body: some View {
+        switch selectedSide {
+        case .light:
+            HeroImageView(heroName: "anakin")
+        case .grey:
+            HeroImageView(heroName: "ahsoka")
+        case .dark:
+            HeroImageView(heroName: "vader")
+        }
+    }
+}
+
+struct HeroImageView : View {
+    var heroName: String
+    
+    var body: some View {
+        Image(heroName)
+            .resizable()
+            .frame(width: 250, height: 400)
+            .shadow(color: .white, radius: 100)
     }
 }
 
